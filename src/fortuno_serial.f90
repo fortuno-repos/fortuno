d2306a73d433aff4af1c3c0091caf7df22abf0a3
Bálint Aradi <aradi@uni-bremen.de>
2025-09-19 19:11:14 +0200

    Minor cleanups in serial, mpi and coa interfaces

--- src/fortuno_serial.f90
+++ src/fortuno_serial.f90
@@ -8,6 +8,7 @@ module fortuno_serial
   use fortuno_serial_serialbasetypes, only : serial_case_base, serial_suite_base
   use fortuno_serial_serialcmdapp, only : execute_serial_cmd_app, init_serial_cmd_app,&
       & serial_cmd_app
+  use fortuno_serial_serialcontext, only : serial_context
   use fortuno_serial_serialglobalctx, only : serial_check, serial_check_failed, serial_failed,&
       & serial_scope_pointers, serial_skip, serial_skipped, serial_store_state
   use fortuno_serial_serialcase, only : serial_case, serial_case_item
